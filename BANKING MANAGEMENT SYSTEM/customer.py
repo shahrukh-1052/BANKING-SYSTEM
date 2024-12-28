5d40a7dc5a6715f25d521480d026f14242f0927c
MOHAMMED HABEEBUDDIN <tufgamerz56@gmail.com>
2024-12-28 15:55:51 +0530

    Initial commit

---
+++ BANKING MANAGEMENT SYSTEM/customer.py
@@ -0,0 +1,16 @@
+#Customer Details
+from database import *
+class Customer:
+
+    def __init__(self, username, password, name, age, city, account_number):
+        self.__username = username
+        self.__password = password
+        self.__name = name
+        self.__age = age
+        self.__city = city
+        self.__account_number = account_number
+
+    def createuser(self):
+        db_query(f"INSERT INTO customers (username, password, name, age, city, account_number, status) VALUES ('{self.__username}', '{self.__password}', '{self.__name}', '{self.__age}', '{self.__city}', '{self.__account_number}', 1);")
+        mydb.commit()
+        
